744ea261aee7736fcfc2e6764e54418b4b7d86d1
Akshat Gadhwal <akshatgadhwal1@gmail.com>
2023-09-02 08:42:13 +0530

    vercel analytics added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
 
+import { inject } from '@vercel/analytics';
+ 
+inject();
+
 const ele = React.createElement('h1', null, 'Hello World!');
 
 console.log(ele);
